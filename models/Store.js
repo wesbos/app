519163b1a3a40a90300160c2acceec2de1c631b3
wesbos <wesbos@gmail.com>
2017-02-22 13:58:54 -0500

    snails

--- models/Store.js
+++ models/Store.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const slug = require('slug');
+const slug = require('slugs');
 
 const storeSchema = new mongoose.Schema({
   name: {
