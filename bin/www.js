7de02e0a559be3e8f28e934c758c0f9b8391a4f9
wesbos <wesbos@gmail.com>
2017-02-28 16:10:32 -0500

    enforce node versions

--- bin/www.js
+++ bin/www.js
@@ -1,6 +1,14 @@
+console.log(parseFloat(process.versions.node));
+
+if (parseFloat(process.versions.node) >= 7.6) {
+  console.log('🛑 🌮 🐶 💪 💩\nHey You! \n\t ya you! \n\t\tBuster! \n\tYou\'re on an older version of node that doesn\'t support the latest and greatest things we are learning (Async + Await)! Please go to nodejs.org and download version 7.6 or greater. 👌\n ');
+  process.exit();
+}
+
 // import environmental variables from our variables.env file
 require('dotenv').config({ path: 'variables.env' });
 const app = require('../app');
+
 
 app.set('port', process.env.PORT || 7777);
 
