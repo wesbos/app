c195fa42e596d35ac95a71518e9f17c0b3448b88
wesbos <wesbos@gmail.com>
2017-02-28 16:33:45 -0500

    nodemon

--- bin/www.js
+++ bin/www.js
@@ -1,6 +1,6 @@
 console.log(parseFloat(process.versions.node));
 
-if (parseFloat(process.versions.node) >= 7.6) {
+if (parseFloat(process.versions.node) < 7.6) {
   console.log('🛑 🌮 🐶 💪 💩\nHey You! \n\t ya you! \n\t\tBuster! \n\tYou\'re on an older version of node that doesn\'t support the latest and greatest things we are learning (Async + Await)! Please go to nodejs.org and download version 7.6 or greater. 👌\n ');
   process.exit();
 }
